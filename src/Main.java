5916ff60553c613f90c1fa674b76c5494e52d8e5
Alexandar <aleksandar.sobolevscky@yandex.by>
2022-11-13 21:40:29 +0300

    Домашка по 14 теме

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("Введите год, месяц и день:");
         LocalDate localDate = LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
         System.out.println(localDate.getDayOfWeek());
+
         //Написать программу для вывода на экран дату следующего вторника
         System.out.println("Дата ближайшего вторника:");
         LocalDate today = LocalDate.now();
@@ -21,28 +22,32 @@ public class Main {
             today = today.plusDays(1);
         }
         System.out.println(today);
+
         //Написать программу, которая проверяет, что строка начинается буквой “J”или “N” и заканчивается “A”.
         System.out.println("Введите строку");
         Predicate<String> isCorrect = str -> ((str.charAt(0) == 'J') || (str.charAt(0) == 'N')) && (str.charAt(str.length() - 1) == 'A');
         System.out.println(isCorrect.test(scanner.next()));
+
         //Написать лямбда выражение, которое принимает на вход объект типа Box и выводит на консоль сообщение
         // “Отгрузили ящик с весом n” где n – поле в классе которое содержит вес ящика.
         Consumer<Box> weightOfBox = box -> System.out.println("Отгрузили ящик весом " + box.weight);
         weightOfBox.accept(new Box(8));
+
         //Написать лямбда выражение, которое принимает на вход число и возвращает значение “Положительное число”,
         // “Отрицательное число” или  “Ноль”.
         Function<Integer, String> value = chislo -> {
             String result;
             if (chislo > 0) {
                 result = "Положительное";
-            } else if (chislo == 0) {
-                result = "Ноль";
-            } else {
+            } else if (chislo < 0) {
                 result = "Отрицательное";
+            } else {
+                result = "Ноль";
             }
             return result;
         };
         System.out.println(value.apply(11));
+
         //Написать лямбда выражение, которое возвращает случайное число от 0 до 10.
         Supplier<Integer> randomInt = () -> new Random().nextInt(0, 11);
         System.out.println(randomInt.get());
